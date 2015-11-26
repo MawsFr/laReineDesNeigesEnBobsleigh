649aaf04147aca3c4735f11eaf4133e08ba1e255
Togam <margot_six@hotmail.com>
2015-11-26 14:34:03 +0100

    Ajout du plugin KeepOnlyNumbersPlugin

---
+++ Project 4 Plugins/src/plugins/KeepOnlyNumbersPlugin.java
@@ -0,0 +1,15 @@
+package plugins;
+
+public class KeepOnlyNumbersPlugin implements Plugin{
+	
+	@Override
+	public String getLabel() {
+		return "Keep only numbers";
+	}
+	
+	@Override
+	public String transform(String s) {
+		return s.replaceAll("[^\\d.]", "");
+	}
+	
+}
