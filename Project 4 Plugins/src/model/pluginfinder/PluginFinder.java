3917a7676d49559769b7e1daebc70c8b8cc71c92
erwandelbrouque <erwandelbrouque@gmail.com>
2015-12-10 20:11:54 +0100

    rajout du throw illegalArgument si files est vide dans notify pluginFinder

--- Project 4 Plugins/src/model/pluginfinder/PluginFinder.java
+++ Project 4 Plugins/src/model/pluginfinder/PluginFinder.java
@@ -137,6 +137,9 @@ public class PluginFinder extends Observable<List<Plugin>> implements ActionList
 		if(files == null){
 			throw new NullPointerException();
 		}
+		if(files.isEmpty()){
+			throw new IllegalArgumentException();
+		}
 		//TODO : verifier files non null
 		List<Plugin> pluginsFinded = null;
 		try {
